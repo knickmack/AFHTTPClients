9254f3675c58569afabeb318f759a40c6eda0d3f
Nik Macintosh <me@knickmack.com>
2013-02-23 18:30:14 -0800

    Create example for Best Buy API

---
+++ Examples/Best Buy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Best Buy
+//
+//  Created by Nik Macintosh on 2013-02-23.
+//  Copyright (c) 2013 Nik Macintosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
